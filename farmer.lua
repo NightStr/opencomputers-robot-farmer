8721bbcb89f0e67b636524dd7fc1183c2ac7005d
NightStrix <nightstrx@gmail.com>
2020-09-12 15:07:48 +0500

    init

---
+++ farmer.lua
@@ -0,0 +1,40 @@
+robot = require("robot")
+component = require("component")
+geolyzer = component.geolyzer
+turn = {robot.turnLeft, robot.turnRight}
+LOOP_COUNT = 5
+
+function turn(lo)
+    if ((lo - 1) % 2) == 0 then
+        robot.turnLeft()
+    else
+        robot.turnRight()
+    end
+end
+
+while true do
+    for i = 0, 1 do
+        robot.turnAround()
+        for l = 1, LOOP_COUNT do
+            while true do
+                robot.forward()
+                block = geolyzer.analyze(0)
+                if block["hardness"] > 0 then
+                    break
+                end
+                if block["growth"] and block["growth"] == 1 then
+                    robot.swingDown()
+                end
+            end
+            if l ~= LOOP_COUNT then
+                turn(l)
+                robot.forward()
+                turn(l)
+            end
+        end
+    end
+    for slot = 1, robot.inventorySize() do
+        robot.select(slot)
+        robot.drop()
+    end
+end
